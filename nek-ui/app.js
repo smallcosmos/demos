48247e6ad3d736080a70d3c773da83cb97b7c4db
hzlinxingjian <65517293@qq.com>
2017-07-26 11:38:59 +0800

    add tabs test demo

--- nek-ui/app.js
+++ nek-ui/app.js
@@ -9,22 +9,69 @@ define([
         }
         return o1;
     }
+    Number.prototype.mul = function(arg){   
+        var m = 0;
+        var s1 = this.toString();
+        var s2 = arg.toString();   
+        try{
+            m += s1.split('.')[1].length;
+        }catch(e){
+            //
+        }
+        try{
+            m += s2.split('.')[1].length;
+        }catch(e){
+            //
+        } 
+        return Number(s1.replace('.', '')) * Number(s2.replace('.', '')) / Math.pow(10,m);  
+    }
+    //除法   
+    Number.prototype.div = function(arg){   
+        var t1=0;
+        var t2=0;
+        var r1,r2;   
+        try{
+            t1 = this.toString().split('.')[1].length;
+        }catch(e){
+            //
+        }
+        try{
+            t2 = arg.toString().split('.')[1].length;
+        }catch(e){
+            //
+        }
+        with(Math){   
+            r1 = Number(this.toString().replace('.', ''));
+            r2 = Number(arg.toString().replace('.', ''));
+            return (r1/r2) * pow(10, t2-t1);
+        }
+    }
     var component = Regular.extend({
         template: tpl,
         config: function(data){
             this.supr(data);
             extend(data, {
                 nekService: '/backend/pageSelectInput/getListByKeys',
-                current: 'panel',
+                current: 'multi.select',
                 testSource: [{
                     id: 1,
-                    name: "asd"
+                    name: "asd",
+                    children: [{
+                        id: 11,
+                        name: "zxc"
+                    }]
                 }, {
                     id: 2,
                     name: "qwe"
                 }],
-                testValue: 2
+                testValue: 2,
+                condition: {
+                    testValue: 2
+                }
             });
+        },
+        init: function(){
+            //
         },
         goto: function(page){
             this.data.current = page;
@@ -50,9 +97,23 @@ define([
             }
         },
 
-        // ui.form
         sourceCompleted: function(event){
-            console.log("source loader");
+            console.log("source loader, event: ", event);
+        }
+    }).filter({
+        klInputTrans: {
+            get: function(value){
+                if(!parseFloat(value)){
+                    return value;
+                }
+                return Number(value).mul(100);
+            },
+            set: function(value){
+                if(!parseFloat(value)){
+                    return value;
+                }
+                return Number(value).div(100);
+            }
         }
     });
     return component;
