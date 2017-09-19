81e60eb06d7bbb498e2c466ad64ec0a904fedf0d
hzlinxingjian <65517293@qq.com>
2017-09-19 15:23:21 +0800

    add test code

--- easy-codepen/index.js
+++ easy-codepen/index.js
@@ -1,1 +1,5 @@
-widnow.test = 'asd';
+var cheerio = require('cheerio');
+
+var $ = cheerio.load(document.body.innerHTML);
+$('body').prepend('<div class="demo-start"></div>');
+window.template = $('.demo-start').nextUntil('script').html();
