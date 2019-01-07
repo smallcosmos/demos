01d215c552774ee46c90ce75f14ea73753096fa1
linxingjian <65517293@qq.com>
2019-01-07 20:34:57 +0800

    feature:  test pre push

--- git/lib/pre.push.js
+++ git/lib/pre.push.js
@@ -1,5 +1,5 @@
 const { spawn } = require('child_process');
 const path = require('path');
 
-spawn('node', [path.join(__dirname, './email.js')]);
+spawn('node', ['--inspect-brk', path.join(__dirname, './email.js')]);
 process.exit(0);
