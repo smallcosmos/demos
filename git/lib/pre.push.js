03d71c057153cc1f5e4e73a72576787e9bfb73c8
linxingjian <65517293@qq.com>
2019-01-07 20:51:11 +0800

    feature:  test pre push

--- git/lib/pre.push.js
+++ git/lib/pre.push.js
@@ -1,6 +1,7 @@
 // const { spawn } = require('child_process');
 // const path = require('path');
 
+console.log(process.env.HUSKY_GIT_STDIN);
 debugger;
 // spawn('node', ['--inspect-brk', path.join(__dirname, './email.js')]);
 process.exit(1);
